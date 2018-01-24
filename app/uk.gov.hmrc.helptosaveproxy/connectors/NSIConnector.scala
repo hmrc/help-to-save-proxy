5c54d57370e0af77454a638874beee8ccb7773ec
Jacqueline <jacqueline.hubbard@digital.hmrc.gov.uk>
2018-01-24 17:07:59 +0000

    copied main code

---
+++ app/uk.gov.hmrc.helptosaveproxy/connectors/NSIConnector.scala
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosaveproxy.connectors
+
+import javax.inject.{Inject, Singleton}
+
+import cats.data.EitherT
+import com.codahale.metrics.Timer
+import com.google.inject.ImplementedBy
+import play.api.Configuration
+import play.api.http.Status
+import play.api.libs.json.{Format, Json}
+import uk.gov.hmrc.helptosaveproxy.config.AppConfig.{nsiAuthHeaderKey, nsiBasicAuth, nsiCreateAccountUrl}
+import uk.gov.hmrc.helptosaveproxy.config.WSHttpProxy
+import uk.gov.hmrc.helptosaveproxy.connectors.NSIConnector.{SubmissionFailure, SubmissionResult, SubmissionSuccess}
+import uk.gov.hmrc.helptosaveproxy.metrics.Metrics
+import uk.gov.hmrc.helptosaveproxy.metrics.Metrics.nanosToPrettyString
+import uk.gov.hmrc.helptosaveproxy.models.NSIUserInfo.nsiUserInfoFormat
+import uk.gov.hmrc.helptosaveproxy.models.NSIUserInfo
+import uk.gov.hmrc.helptosaveproxy.util.HttpResponseOps._
+import uk.gov.hmrc.helptosaveproxy.util.Logging._
+import uk.gov.hmrc.helptosaveproxy.util.{Logging, NINO, NINOLogMessageTransformer, PagerDutyAlerting, Result, maskNino}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.config.AppName
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@ImplementedBy(classOf[NSIConnectorImpl])
+trait NSIConnector {
+  def createAccount(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[SubmissionResult]
+
+  def updateEmail(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ex: ExecutionContext): Result[Unit]
+
+  def healthCheck(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ex: ExecutionContext): Result[Unit]
+
+}
+
+object NSIConnector {
+
+  sealed trait SubmissionResult
+
+  case class SubmissionSuccess() extends SubmissionResult
+
+  case class SubmissionFailure(errorMessageId: Option[String], errorMessage: String, errorDetail: String) extends SubmissionResult
+
+  implicit val submissionFailureFormat: Format[SubmissionFailure] = Json.format[SubmissionFailure]
+
+}
+
+@Singleton
+class NSIConnectorImpl @Inject() (conf: Configuration, metrics: Metrics, pagerDutyAlerting: PagerDutyAlerting)(
+    implicit
+    transformer: NINOLogMessageTransformer) extends NSIConnector with Logging with AppName {
+
+  val httpProxy: WSHttpProxy = new WSHttpProxy(conf)
+
+  override def createAccount(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[SubmissionResult] = {
+    import uk.gov.hmrc.helptosaveproxy.util.Toggles._
+
+    val nino = userInfo.nino
+
+    logger.info(s"Trying to create an account using NSI endpoint $nsiCreateAccountUrl", nino)
+
+    FEATURE("log-account-creation-json", conf, logger).thenOrElse(
+      logger.info(s"CreateAccount JSON is ${Json.toJson(userInfo)}", nino),
+      ()
+    )
+
+    val timeContext: Timer.Context = metrics.nsiAccountCreationTimer.time()
+
+    httpProxy.post(nsiCreateAccountUrl, userInfo, Map(nsiAuthHeaderKey → nsiBasicAuth))(nsiUserInfoFormat, hc.copy(authorization = None), ec)
+      .map[SubmissionResult] { response ⇒
+        val time = timeContext.stop()
+
+        response.status match {
+          case Status.CREATED ⇒
+            logger.info(s"createAccount/insert returned 201 (Created) ${timeString(time)}", nino)
+            SubmissionSuccess()
+
+          case Status.CONFLICT ⇒
+            logger.info(s"createAccount/insert returned 409 (Conflict). Account had already been created - " +
+              s"proceeding as normal ${timeString(time)}", nino)
+            SubmissionSuccess()
+
+          case other ⇒
+            pagerDutyAlerting.alert("Received unexpected http status in response to create account")
+            handleErrorStatus(other, response, userInfo.nino, time)
+        }
+      }.recover {
+        case e ⇒
+          val time = timeContext.stop()
+          pagerDutyAlerting.alert("Failed to make call to create account")
+          metrics.nsiAccountCreationErrorCounter.inc()
+
+          logger.warn(s"Encountered error while trying to create account ${timeString(time)}", e, nino)
+          SubmissionFailure(None, "Encountered error while trying to create account", e.getMessage)
+      }
+  }
+
+  override def updateEmail(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ec: ExecutionContext): Result[Unit] = EitherT[Future, String, Unit]{
+    val nino = userInfo.nino
+
+    val timeContext: Timer.Context = metrics.nsiUpdateEmailTimer.time()
+
+    httpProxy.put(nsiCreateAccountUrl, userInfo, true, Map(nsiAuthHeaderKey → nsiBasicAuth))(nsiUserInfoFormat, hc.copy(authorization = None), ec)
+      .map[Either[String, Unit]] { response ⇒
+        val time = timeContext.stop()
+
+        response.status match {
+          case Status.OK ⇒
+            logger.info(s"createAccount/update returned 200 OK from NSI ${timeString(time)}", nino)
+            Right(())
+
+          case other ⇒
+            metrics.nsiUpdateEmailErrorCounter.inc()
+            pagerDutyAlerting.alert("Received unexpected http status in response to update email")
+            Left(s"Received unexpected status $other from NS&I while trying to update email ${timeString(time)}. " +
+              s"Body was ${maskNino(response.body)}")
+
+        }
+      }.recover {
+        case e ⇒
+          val time = timeContext.stop()
+          pagerDutyAlerting.alert("Failed to make call to update email")
+          metrics.nsiUpdateEmailErrorCounter.inc()
+
+          Left(s"Encountered error while trying to create account: ${e.getMessage} ${timeString(time)}")
+      }
+  }
+
+  override def healthCheck(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ex: ExecutionContext): Result[Unit] = EitherT[Future, String, Unit]{
+    httpProxy.put(nsiCreateAccountUrl, userInfo, false, Map(nsiAuthHeaderKey → nsiBasicAuth))(nsiUserInfoFormat, hc.copy(authorization = None), ex)
+      .map[Either[String, Unit]] { response ⇒
+        response.status match {
+          case Status.OK ⇒ Right(())
+          case other     ⇒ Left(s"Received unexpected status $other from NS&I while trying to do health-check. Body was ${maskNino(response.body)}")
+        }
+      }.recover {
+        case e ⇒ Left(s"Encountered error while trying to create account: ${e.getMessage}")
+      }
+  }
+
+  private def handleErrorStatus(status: Int, response: HttpResponse, nino: NINO, time: Long) = {
+    metrics.nsiAccountCreationErrorCounter.inc()
+
+    status match {
+      case Status.BAD_REQUEST ⇒
+        logger.warn(s"Failed to create account as NSI, received status 400 (Bad Request) from NSI ${timeString(time)}", nino)
+        handleError(response)
+
+      case Status.INTERNAL_SERVER_ERROR ⇒
+        logger.warn(s"Failed to create account as NSI, received status 500 (Internal Server Error) from NSI ${timeString(time)}", nino)
+        handleError(response)
+
+      case Status.SERVICE_UNAVAILABLE ⇒
+        logger.warn(s"Failed to create account as NSI, received status 503 (Service Unavailable) from NSI ${timeString(time)}", nino)
+        handleError(response)
+
+      case other ⇒
+        logger.warn(s"Unexpected error during creating account, received status $other ${timeString(time)}", nino)
+        handleError(response)
+    }
+  }
+
+  private def timeString(nanos: Long): String = s"(round-trip time: ${nanosToPrettyString(nanos)})"
+
+  private def handleError(response: HttpResponse): SubmissionFailure = {
+    logger.warn(s"response body from NSI=${maskNino(response.body)}")
+    response.parseJSON[SubmissionFailure](Some("error")) match {
+      case Right(submissionFailure) ⇒ submissionFailure
+      case Left(error)              ⇒ SubmissionFailure(None, "", error)
+    }
+  }
+}
---
+++ app/uk.gov.hmrc.helptosaveproxy/models/NSIUserInfo.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosaveproxy.models
+
+import cats.instances.string._
+import cats.syntax.eq._
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import play.api.libs.json._
+import uk.gov.hmrc.helptosaveproxy.models.NSIUserInfo.ContactDetails
+
+import scala.util.{Failure, Success, Try}
+
+case class NSIUserInfo(forename:            String,
+                       surname:             String,
+                       dateOfBirth:         LocalDate,
+                       nino:                String,
+                       contactDetails:      ContactDetails,
+                       registrationChannel: String         = "online")
+
+object NSIUserInfo {
+
+  implicit class NSIUserInfoOps(val nsiUserInfo: NSIUserInfo) {
+    def updateEmail(newEmail: String): NSIUserInfo =
+      nsiUserInfo.copy(contactDetails = nsiUserInfo.contactDetails.copy(email = newEmail))
+  }
+
+  case class ContactDetails(address1:                String,
+                            address2:                String,
+                            address3:                Option[String],
+                            address4:                Option[String],
+                            address5:                Option[String],
+                            postcode:                String,
+                            countryCode:             Option[String],
+                            email:                   String,
+                            phoneNumber:             Option[String] = None,
+                            communicationPreference: String         = "02")
+
+  private implicit class StringOps(val s: String) {
+    def removeAllSpaces: String = s.replaceAll(" ", "")
+
+    def cleanupSpecialCharacters: String = s.replaceAll("\t|\n|\r", " ").trim.replaceAll("\\s{2,}", " ")
+
+  }
+
+  /**
+   * Performs validation checks on the given [[UserInfo]] and converts to [[NSIUserInfo]]
+   * if successful.
+   */
+  def apply(userInfo: UserInfo, email: String): NSIUserInfo = {
+      def extractContactDetails(userInfo: UserInfo): ContactDetails = {
+        val (line1, line2, line3, line4, line5) =
+          userInfo.address.lines.map(_.cleanupSpecialCharacters).filter(_.nonEmpty) match {
+            case Nil ⇒ ("", "", None, None, None)
+            case line1 :: Nil ⇒ (line1, "", None, None, None)
+            case line1 :: line2 :: Nil ⇒ (line1, line2, None, None, None)
+            case line1 :: line2 :: line3 :: Nil ⇒ (line1, line2, Some(line3), None, None)
+            case line1 :: line2 :: line3 :: line4 :: Nil ⇒ (line1, line2, Some(line3), Some(line4), None)
+            case line1 :: line2 :: line3 :: line4 :: line5 :: Nil ⇒ (line1, line2, Some(line3), Some(line4), Some(s"$line5"))
+            case line1 :: line2 :: line3 :: line4 :: line5 :: other ⇒ (line1, line2, Some(line3), Some(line4), Some(s"$line5,${other.mkString(",")}"))
+          }
+
+        ContactDetails(
+          line1, line2, line3, line4, line5,
+          userInfo.address.postcode.getOrElse("").cleanupSpecialCharacters.removeAllSpaces,
+          userInfo.address.country.map(_.cleanupSpecialCharacters.removeAllSpaces).filter(_.toLowerCase =!= "other").map(_.take(2)),
+          email
+        )
+      }
+
+    NSIUserInfo(
+      userInfo.forename.cleanupSpecialCharacters,
+      userInfo.surname.cleanupSpecialCharacters,
+      userInfo.dateOfBirth,
+      userInfo.nino.cleanupSpecialCharacters.removeAllSpaces,
+      extractContactDetails(userInfo)
+    )
+  }
+
+  implicit val dateFormat: Format[LocalDate] = new Format[LocalDate] {
+    val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+
+    override def writes(o: LocalDate): JsValue = JsString(o.format(formatter))
+
+    override def reads(json: JsValue): JsResult[LocalDate] = json match {
+      case JsString(s) ⇒ Try(LocalDate.parse(s, formatter)) match {
+        case Success(date)  ⇒ JsSuccess(date)
+        case Failure(error) ⇒ JsError(s"Could not parse date as yyyyMMdd: ${error.getMessage}")
+      }
+
+      case other ⇒ JsError(s"Expected string but got $other")
+    }
+  }
+
+  implicit val contactDetailsFormat: Format[ContactDetails] = Json.format[ContactDetails]
+
+  implicit val nsiUserInfoFormat: Format[NSIUserInfo] = Json.format[NSIUserInfo]
+
+}
