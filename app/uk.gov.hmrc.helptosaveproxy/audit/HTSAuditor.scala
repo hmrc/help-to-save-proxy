858531baff666bc9e9d0b9e85016be187c65c328
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-05-09 12:35:46 +0100

    fix

--- app/uk.gov.hmrc.helptosaveproxy/audit/HTSAuditor.scala
+++ app/uk.gov.hmrc.helptosaveproxy/audit/HTSAuditor.scala
@@ -19,14 +19,14 @@ package uk.gov.hmrc.helptosaveproxy.audit
 import javax.inject.{Inject, Singleton}
 import uk.gov.hmrc.helptosaveproxy.models.HTSEvent
 import uk.gov.hmrc.helptosaveproxy.util.Logging.LoggerOps
-import uk.gov.hmrc.helptosaveproxy.util.{LogMessageTransformer, Logging, NINO, WithMdcExecutionContext}
+import uk.gov.hmrc.helptosaveproxy.util.{LogMessageTransformer, Logging, NINO}
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 
 import scala.concurrent.ExecutionContext
 import scala.util.control.NonFatal
 
 @Singleton
-class HTSAuditor @Inject() (val auditConnector: AuditConnector)(implicit transformer: LogMessageTransformer) extends Logging with WithMdcExecutionContext {
+class HTSAuditor @Inject() (val auditConnector: AuditConnector)(implicit transformer: LogMessageTransformer) extends Logging {
 
   def sendEvent(event: HTSEvent, nino: NINO, correlationId: Option[String])(implicit ec: ExecutionContext): Unit = {
     val checkEventResult = auditConnector.sendEvent(event.value)
