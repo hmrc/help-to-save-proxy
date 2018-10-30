ab61c539d51af9b0afbbe36ae8b276406356e055
Jacqueline Hubbard <Jackie1988@users.noreply.github.com>
2018-10-30 13:02:29 +0000

    HTS-1100 added pattern regex for accountNumber to only allow digits (#66)

--- test/uk/gov/hmrc/helptosaveproxy/services/JSONSchemaValidationServiceImplSpec.scala
+++ test/uk/gov/hmrc/helptosaveproxy/services/JSONSchemaValidationServiceImplSpec.scala
@@ -284,6 +284,18 @@ class JSONSchemaValidationServiceImplSpec extends TestSupport {
     "when given a NSIPayload that the json validation schema reports that the rollNumber is invalid, return a message" in {
       testError(validNSIPayloadJSON.replace(Fields.nbaDetails, Json.toJson(BankDetails("12-34-56", "12345678", Some("a b & c !@_"), "account name"))))
     }
+
+    "when given a NSIPayload that the json validation schema reports that the accountNumber is invalid - too short, return a message" in {
+      testError(validNSIPayloadJSON.replace(Fields.nbaDetails, Json.toJson(BankDetails("12-34-56", "", Some("897/98X"), "account name"))))
+    }
+
+    "when given a NSIPayload that the json validation schema reports that the accountNumber is invalid - too long, return a message" in {
+      testError(validNSIPayloadJSON.replace(Fields.nbaDetails, Json.toJson(BankDetails("12-34-56", "123456789", Some("897/98X"), "account name"))))
+    }
+
+    "when given a NSIPayload that the json validation schema reports that the accountNumber is invalid - it contains a non digit, return a message" in {
+      testError(validNSIPayloadJSON.replace(Fields.nbaDetails, Json.toJson(BankDetails("12-34-56", "1234567w", Some("897/98X"), "account name"))))
+    }
   }
 
 }
