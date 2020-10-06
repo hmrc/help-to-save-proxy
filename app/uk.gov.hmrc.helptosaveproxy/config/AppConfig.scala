a1dea7d0bc0fb9e60fea391c52f29711ffe98bd3
Dave English <4254983+lutnos@users.noreply.github.com>
2020-10-06 15:56:09 +0100

    DLS-2362 mend log level config determination

--- app/uk.gov.hmrc.helptosaveproxy/config/AppConfig.scala
+++ app/uk.gov.hmrc.helptosaveproxy/config/AppConfig.scala
@@ -32,18 +32,17 @@ class AppConfig @Inject()(val runModeConfiguration: Configuration, sc: ServicesC
 
   def getString(key: String): String = sc.getString(key)
 
-  def logLevel: String = sc.getConfString("logger.help-to-save-proxy", "none")
+  val logLevel: String = getString("logger.help-to-save-proxy")
 
-  val appName: String = sc.getString("appName")
+  val appName: String = getString("appName")
 
-  val nsiAuthHeaderKey: String = sc.getString("microservice.services.nsi.client.httpheader.header-key")
+  val nsiAuthHeaderKey: String = getString("microservice.services.nsi.client.httpheader.header-key")
 
   val nsiBasicAuth: String = {
-    val user = new String(
-      base64Decode(sc.getString("microservice.services.nsi.client.httpheader.basicauth.Base64User")))
+    val user = new String(base64Decode(getString("microservice.services.nsi.client.httpheader.basicauth.Base64User")))
     val password = new String(
-      base64Decode(sc.getString("microservice.services.nsi.client.httpheader.basicauth.Base64Password")))
-    val encoding = sc.getString("microservice.services.nsi.client.httpheader.encoding")
+      base64Decode(getString("microservice.services.nsi.client.httpheader.basicauth.Base64Password")))
+    val encoding = getString("microservice.services.nsi.client.httpheader.encoding")
 
     s"Basic ${new String(base64Encode(s"$user:$password"), encoding)}"
   }
@@ -52,7 +51,7 @@ class AppConfig @Inject()(val runModeConfiguration: Configuration, sc: ServicesC
 
   val nsiQueryAccountUrl: String = s"${sc.baseUrl("nsi")}/nsi-services"
 
-  val systemId: String = sc.getString("microservice.services.dwp.system-id")
+  val systemId: String = getString("microservice.services.dwp.system-id")
 
   val dwpBaseUrl: String = sc.baseUrl("dwp")
 
