17663b971eeb24bb985ba2b163ae2ca5e4a80a7b
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2021-09-15 15:56:37 +0100

    [JR][DLS-4419]: Play 2.8 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,12 @@ import sbt._
 
 object AppDependencies {
 
-  val hmrc = "uk.gov.hmrc"
-
   val compile = Seq(
     ws,
-    hmrc                %% "domain"                     % "6.2.0-play-28",
-    hmrc                %% "mongo-lock"                 % "7.0.0-play-26",
-    hmrc                %% "bootstrap-backend-play-28"  % "5.10.0",
-    hmrc                %% "simple-reactivemongo"       % "8.0.0-play-26",
+    "uk.gov.hmrc"       %% "domain"                     % "6.2.0-play-28",
+    "uk.gov.hmrc"       %% "mongo-lock"                 % "7.0.0-play-28",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"  % "5.10.0",
+    "uk.gov.hmrc"       %% "simple-reactivemongo"       % "8.0.0-play-28",
     "com.eclipsesource" %% "play-json-schema-validator" % "0.9.4",
     "org.typelevel"     %% "cats-core"                  % "2.5.0",
     "com.github.kxbmap" %% "configs"                     % "0.6.0",
@@ -20,12 +18,12 @@ object AppDependencies {
   )
 
   val test = Seq(
-    hmrc                   %% "service-integration-test"    % "1.1.0-play-26"     % "test",
-    hmrc                   %% "stub-data-generator"         % "0.5.3"             % "test",
+    "uk.gov.hmrc"          %% "service-integration-test"    % "1.1.0-play-28"     % "test",
+    "uk.gov.hmrc"          %% "stub-data-generator"         % "0.5.3"             % "test",
     "org.scalatest"        %% "scalatest"                   % "3.2.8"             % "test",
-    "com.vladsch.flexmark" % "flexmark-all"                   % "0.35.10"           % "test",
+    "com.vladsch.flexmark"  %  "flexmark-all"                 % "0.35.10"           % "test",
     "org.scalatestplus"    %% "scalatestplus-scalacheck"    % "3.1.0.0-RC2"       % "test",
-    "org.pegdown"          % "pegdown"                      % "1.6.0"             % "test",
+    "org.pegdown"          %  "pegdown"                     % "1.6.0"             % "test",
     "com.typesafe.play"    %% "play-test"                   % PlayVersion.current % "test",
     "org.scalamock"        %% "scalamock-scalatest-support" % "3.6.0"             % "test",
     "com.miguno.akka"      %% "akka-mock-scheduler"         % "0.5.5"             % "test",
