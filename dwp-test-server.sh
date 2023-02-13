1dea3bd04aa839d8d2f2300f65c15f5603e6d364
lutnos <4254983+lutnos@users.noreply.github.com>
2023-02-13 10:34:21 +0000

    DLS-6839 revert adding local testing self-signed certificates

--- dwp-test-server.sh
+++
@@ -1,2 +0,0 @@
-# password is "dspw"
-openssl s_server -key conf/certificates/dwpServerKey.pem -cert conf/certificates/dwpServerCertificate.pem -accept 1443 -www -msg -state -debug
--- nsandi-test-server.sh
+++
@@ -1,2 +0,0 @@
-# password is "nspw"
-openssl s_server -key conf/certificates/nsandiServerKey.pem -cert conf/certificates/nsandiServerCertificate.pem -accept 2443 -www -msg -state -debug
