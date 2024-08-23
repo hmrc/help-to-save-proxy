59220558509b804e3395cd0df7d2751ab47adf88
johno1985 <1640705+johno1985@users.noreply.github.com>
2024-08-23 16:42:05 +0100

    DLS-10868: Use scalacheck-1-17 instead of RC version of scalatestplus-scalacheck

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,14 +10,14 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-$playVersion"        % hmrcMongoVersion,
     "com.github.java-json-tools" %% "json-schema-validator"           % "2.2.14",
     "org.typelevel"              %% "cats-core"                       % "2.9.0",
-    "com.github.kxbmap"          %% "configs"                         % "0.6.1",
+    "com.github.kxbmap"          %% "configs"                         % "0.6.1"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"          %% s"bootstrap-test-$playVersion"  % bootstrapVersion % "test",
-    "org.scalatestplus"    %% "scalatestplus-scalacheck"      % "3.1.0.0-RC2"    % "test",
-    "org.scalamock"        %% "scalamock"                     % "5.2.0"          % "test",
-    "com.github.pjfanning" %% "pekko-mock-scheduler"          % "0.6.0"          % "test",
-    "org.apache.pekko"     %% "pekko-testkit"                 % "1.0.2"          % "test"
+    "uk.gov.hmrc"          %% s"bootstrap-test-$playVersion" % bootstrapVersion % "test",
+    "org.scalatestplus"    %% "scalacheck-1-17"              % "3.2.18.0"       % "test",
+    "org.scalamock"        %% "scalamock"                    % "5.2.0"          % "test",
+    "com.github.pjfanning" %% "pekko-mock-scheduler"         % "0.6.0"          % "test",
+    "org.apache.pekko"     %% "pekko-testkit"                % "1.0.2"          % "test"
   )
 }
